b7421e3315079c81428f794740fdc8215b8625a2
Fenikz <dsmakarov@gmail.com>
2016-04-06 22:13:10 +0300

    with start screen

--- core/src/com/sirpomidor/drop/Drop.java
+++ core/src/com/sirpomidor/drop/Drop.java
@@ -1,138 +1,33 @@
 package com.sirpomidor.drop;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.TimeUtils;
-import com.badlogic.gdx.math.Rectangle;
 
-import java.util.Iterator;
-
-public class Drop extends ApplicationAdapter {
+/**
+ * Created by Dmitry on 06.04.2016.
+ */
+public class Drop extends Game {
 
     SpriteBatch batch;
-    OrthographicCamera camera;
+    BitmapFont font;
 
-    Texture dropImage;
-	Texture bucketImage;
-	Sound dropSound;
-	Music rainMusic;
-    Rectangle bucket;
-    Vector3 touchPos;
-    Array<Rectangle> raindrops;
-    long lastDropTime;
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
 
-	@Override
-	public void create () {
-
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
-
-		batch = new SpriteBatch();
-
-        dropImage = new Texture("poo.png");
-        bucketImage = new Texture("bucket.png");
-
-        dropSound = Gdx.audio.newSound(Gdx.files.internal("rapide.wav"));
-        rainMusic = Gdx.audio.newMusic(Gdx.files.internal("fantazer.mp3"));
-
-        rainMusic.setLooping(true);
-        rainMusic.play();
-
-        bucket = new Rectangle();
-        bucket.x = 800 / 2 - 64 /2;
-        bucket.y = 20;
-        bucket.width = 64;
-        bucket.height = 64;
-
-        touchPos = new Vector3();
-
-        raindrops = new Array<Rectangle>();
-        spawnRaindrop();
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        camera.update();
-
-        batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y);
-        for (Rectangle raindrop : raindrops) {
-            batch.draw(dropImage, raindrop.x, raindrop.y);
-        }
-		batch.end();
-
-        if (Gdx.input.isTouched()) {
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            camera.unproject(touchPos);
-            bucket.x = (int) touchPos.x - 64 / 2;
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            bucket.x += 200 * Gdx.graphics.getDeltaTime();
-        }
-
-        if (bucket.x < 0) {
-            bucket.x = 0;
-        }
-
-        if (bucket.x > 800 - 64) {
-            bucket.x = 800 - 64;
-        }
-
-        if (TimeUtils.nanoTime() - lastDropTime > 1000000000) {
-            spawnRaindrop();
-        }
-
-        Iterator<Rectangle> iter = raindrops.iterator();
-
-        while (iter.hasNext()) {
-            Rectangle raindrop = iter.next();
-            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-            if (raindrop.y + 64 < 0) {
-                iter.remove();
-            }
-            if (raindrop.overlaps(bucket)) {
-                dropSound.play();
-                iter.remove();
-            }
-        }
-	}
-
-    private void spawnRaindrop() {
-        Rectangle raindrop = new Rectangle();
-        raindrop.x = MathUtils.random(0, 800 - 64);
-        raindrop.y = 480;
-        raindrop.width = 64;
-        raindrop.height = 64;
-        raindrops.add(raindrop);
-        lastDropTime = TimeUtils.nanoTime();
+    @Override
+    public void render() {
+        super.render();
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        dropSound.dispose();
-        dropImage.dispose();
-        bucketImage.dispose();
-        rainMusic.dispose();
         batch.dispose();
+        font.dispose();
     }
 }
--- html/src/com/sirpomidor/drop/client/HtmlLauncher.java
+++ html/src/com/sirpomidor/drop/client/HtmlLauncher.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import com.sirpomidor.drop.Drop;
+import com.sirpomidor.drop.GameScreen;
 
 public class HtmlLauncher extends GwtApplication {
 
